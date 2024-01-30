6e8aa40ee0f6ff5e4b1fc363a735fa1b2ba0a18d
kolom <s_kolomeetzc@ukr.net>
2024-01-30 07:22:07 +0100

    Homework finish

--- main.py
+++ main.py
@@ -2,27 +2,27 @@ print("Hello world")
 
 ################# 1 ###############
 
-# def power(base, exp):
-#     if (exp == 1):
-#         return (base)
-#     if (exp != 1):
-#         return (base * power(base, exp - 1))
-# base = int(input("Введіть число: "))
-# exp = int(input("Введіть його ступінь: "))
-# print("Результат зведення в ступінь дорівнює:", power(base, exp))
+def power(base, exp):
+    if (exp == 1):
+        return (base)
+    if (exp != 1):
+        return (base * power(base, exp - 1))
+base = int(input("Введіть число: "))
+exp = int(input("Введіть його ступінь: "))
+print("Результат зведення в ступінь дорівнює:", power(base, exp))
 
-############### 2 ##############
+############## 2 ##############
 
-# input sumCount(int value)
-# {
-#     if (value <= 0)
-#         return 0; // базовий випадок (умова завершення)
-#     else if (value == 1)
-#         return 1; // базовий випадок (умова завершення)
-#     else
-#         return sumCount(value - 1) + value; // рекурсивний виклик функції
-#
-# }
+input sumCount(int value)
+{
+    if (value <= 0)
+        return 0; // базовий випадок (умова завершення)
+    else if (value == 1)
+        return 1; // базовий випадок (умова завершення)
+    else
+        return sumCount(value - 1) + value; // рекурсивний виклик функції
+
+}
 
 ################ 3 ################
 
@@ -31,3 +31,5 @@ def sum_range(a, b):
         return 0
     return a + sum_range(a+1, b)
 print(sum_range(1, 5))
+
+
