e18f14329dacd05a6a097fad5aaf640b5ad97a7d
kolom <s_kolomeetzc@ukr.net>
2024-01-30 06:54:55 +0100

    Homework begin the first finish

--- main.py
+++ main.py
@@ -2,6 +2,13 @@ print("Hello world")
 
 ################# 1 ###############
 
-
+def power(base, exp):
+    if (exp == 1):
+        return (base)
+    if (exp != 1):
+        return (base * power(base, exp - 1))
+base = int(input("Введіть число: "))
+exp = int(input("Введіть його ступінь: "))
+print("Результат зведення в ступінь дорівнює:", power(base, exp))
 
 
